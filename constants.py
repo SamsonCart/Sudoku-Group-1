67adaed0f4fa4a4ed9d9962008543ca64364fe19
Samson Carter <129422671+SamsonCart@users.noreply.github.com>
2023-07-25 08:18:10 -0400

    Update constants.py
    Modified constants to suit needs for draw(self) function

--- constants.py
+++ constants.py
@@ -1,18 +1,24 @@
-WIDTH = 600
-HEIGHT = 600
-LINE_WIDTH = 15
-WIN_LINE_WIDTH = 15
-BOARD_ROWS = 3
-BOARD_COLS = 3
-SQUARE_SIZE = 200
+WIDTH = 900 # adjusted width and height from 600 to 900 for easier math
+HEIGHT = 900
+LINE_WIDTH = 10 # reduced line width to 10 for easier distinction between main and secondary lines
+MAIN_LINE_WIDTH = 20 # Changed named from Win_Line_Width to Main_Line_Width and increased to 20 for a larger board
+BOARD_ROWS = 9
+BOARD_COLS = 9
+SQUARE_SIZE = 100 # adjusted square size to 100 for easier math
+SPACE = 55
+
+BG_COLOR = (255, 255, 245)
+LINE_COLOR = (66, 66, 66) # Changed line color to black based on CROSS_COLOR
+
+CHIP_FONT = 400
+GAME_OVER_FONT = 40
+
+"""
+Removed: 
 CIRCLE_RADIUS = 60
 CIRCLE_WIDTH = 15
 CROSS_WIDTH = 25
-SPACE = 55
 RED = (255, 0, 0)
-BG_COLOR = (255, 255, 245)
-LINE_COLOR = (245, 152, 66)
 CIRCLE_COLOR = (155, 155, 155)
 CROSS_COLOR = (66, 66, 66)
-CHIP_FONT = 400
-GAME_OVER_FONT = 40
+"""
