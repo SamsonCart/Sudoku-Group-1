bae1034d24eb91403f6551b2fa720946260984d0
dylanrdixon <139259834+dylanrdixon@users.noreply.github.com>
2023-08-01 19:40:42 -0400

    Update constants.py
    Group updates

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 WIDTH = 900 # adjusted width and height from 600 to 900 for easier math
-HEIGHT = 900
+HEIGHT = 1000
 LINE_WIDTH = 10 # reduced line width to 10 for easier distinction between main and secondary lines
 MAIN_LINE_WIDTH = 20 # Changed named from Win_Line_Width to Main_Line_Width and increased to 20 for a larger board
+WIN_LINE_WIDTH = 15
 BOARD_ROWS = 9
 BOARD_COLS = 9
 SQUARE_SIZE = 100 # adjusted square size to 100 for easier math
@@ -11,16 +12,12 @@ BG_COLOR = (255, 255, 245)
 LINE_COLOR = (66, 66, 66) # Changed line color to black based on CROSS_COLOR
 NUMBER_COLOR = (66, 66, 66) # Added number color constant
 SKETCHED_NUMBER_COLOR = (128, 128, 128) # Added sketched number color constant
+SELECTED_COLOR = (255, 0, 0)
+BG_COLOR_IN_GAME = (156, 200, 255) #created a different color for the GAME-IN-PROGRESS screen
+LINE_COLOR_ORANGE = (245, 152, 66)  #color orange
+LINE_COLOR_BLACK = (33, 33, 33) #color black
+LINE_COLOR_GRAY = (119, 122, 122) #color gray
 
-CHIP_FONT = 400
 GAME_OVER_FONT = 40
-
-"""
-Removed: 
-CIRCLE_RADIUS = 60
-CIRCLE_WIDTH = 15
-CROSS_WIDTH = 25
-RED = (255, 0, 0)
-CIRCLE_COLOR = (155, 155, 155)
-CROSS_COLOR = (66, 66, 66)
-"""
+NUMBER_FONT = 100
+SKETCHED_CHIP_FONT = 65
