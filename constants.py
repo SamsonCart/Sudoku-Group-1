5c350128009bbd00cfaabeefd82480746f926373
Chance Nahuway <ChanceNahuway@Nahuway-MacBook-Pro.local>
2023-08-02 05:20:12 -0500

    Added file header

--- constants.py
+++ constants.py
@@ -1,3 +1,11 @@
+"""
+This file contains constants that are used in the sudoku game.
+
+Authors: Samson Carter, Chance Nahuway, Dylan Dixon, Diamond Nicholas
+Project 4, Group 1
+UF COP 3502C in Summer 2023.
+"""
+
 # Dimensional constants
 WIDTH = 900
 HEIGHT = 1000
