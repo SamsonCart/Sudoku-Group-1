313a7dd2bce27a5dfdc3c48051626e88ef0665f9
Samson Carter <129422671+SamsonCart@users.noreply.github.com>
2023-08-02 20:58:41 -0400

    Update cell.py
    Update cell class to correctly associate the proper location for the rect

--- cell.py
+++ cell.py
@@ -47,43 +47,43 @@ class Cell:
 
         if self.value == 1:
             cell_1_rect = cell_1_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(cell_1_surf, cell_1_rect)
         elif self.value == 2:
             cell_2_rect = cell_2_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(cell_2_surf, cell_2_rect)
         elif self.value == 3:
             cell_3_rect = cell_3_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(cell_3_surf, cell_3_rect)
         elif self.value == 4:
             cell_4_rect = cell_4_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(cell_4_surf, cell_4_rect)
         elif self.value == 5:
             cell_5_rect = cell_5_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(cell_5_surf, cell_5_rect)
         elif self.value == 6:
             cell_6_rect = cell_6_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(cell_6_surf, cell_6_rect)
         elif self.value == 7:
             cell_7_rect = cell_7_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(cell_7_surf, cell_7_rect)
         elif self.value == 8:
             cell_8_rect = cell_8_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(cell_8_surf, cell_8_rect)
         elif self.value == 9:
             cell_9_rect = cell_9_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(cell_9_surf, cell_9_rect)
         else:
             cell_blank_rect = cell_blank_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(cell_blank_surf, cell_blank_rect)
 
         num_font = pygame.font.Font(None, NUMBER_FONT)
@@ -102,49 +102,49 @@ class Cell:
 
         if self.sketched_value == 1:
             sketched_cell_1_rect = sketched_cell_1_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 4, self.row * SQUARE_SIZE + SQUARE_SIZE // 3))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 4, self.col * SQUARE_SIZE + SQUARE_SIZE // 3))
             self.screen.blit(sketched_cell_1_surf, sketched_cell_1_rect)
         elif self.sketched_value == 2:
             sketched_cell_2_rect = sketched_cell_2_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 4, self.row * SQUARE_SIZE + SQUARE_SIZE // 3))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 4, self.col * SQUARE_SIZE + SQUARE_SIZE // 3))
             self.screen.blit(sketched_cell_2_surf, sketched_cell_2_rect)
         elif self.sketched_value == 3:
             sketched_cell_3_rect = sketched_cell_3_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 4, self.row * SQUARE_SIZE + SQUARE_SIZE // 3))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 4, self.col * SQUARE_SIZE + SQUARE_SIZE // 3))
             self.screen.blit(sketched_cell_3_surf, sketched_cell_3_rect)
         elif self.sketched_value == 4:
             sketched_cell_4_rect = sketched_cell_4_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 4, self.row * SQUARE_SIZE + SQUARE_SIZE // 3))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 4, self.col * SQUARE_SIZE + SQUARE_SIZE // 3))
             self.screen.blit(sketched_cell_4_surf, sketched_cell_4_rect)
         elif self.sketched_value == 5:
             sketched_cell_5_rect = sketched_cell_5_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 4, self.row * SQUARE_SIZE + SQUARE_SIZE // 3))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 4, self.col * SQUARE_SIZE + SQUARE_SIZE // 3))
             self.screen.blit(sketched_cell_5_surf, sketched_cell_5_rect)
         elif self.sketched_value == 6:
             sketched_cell_6_rect = sketched_cell_6_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 4, self.row * SQUARE_SIZE + SQUARE_SIZE // 3))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 4, self.col * SQUARE_SIZE + SQUARE_SIZE // 3))
             self.screen.blit(sketched_cell_6_surf, sketched_cell_6_rect)
         elif self.sketched_value == 7:
             sketched_cell_7_rect = sketched_cell_7_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 4, self.row * SQUARE_SIZE + SQUARE_SIZE // 3))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 4, self.col * SQUARE_SIZE + SQUARE_SIZE // 3))
             self.screen.blit(sketched_cell_7_surf, sketched_cell_7_rect)
         elif self.sketched_value == 8:
             sketched_cell_8_rect = sketched_cell_8_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 4, self.row * SQUARE_SIZE + SQUARE_SIZE // 3))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 4, self.col * SQUARE_SIZE + SQUARE_SIZE // 3))
             self.screen.blit(sketched_cell_8_surf, sketched_cell_8_rect)
         elif self.sketched_value == 9:
             sketched_cell_9_rect = sketched_cell_9_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 4, self.row * SQUARE_SIZE + SQUARE_SIZE // 3))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 4, self.col * SQUARE_SIZE + SQUARE_SIZE // 3))
             self.screen.blit(sketched_cell_9_surf, sketched_cell_9_rect)
         else:
             sketched_cell_blank_rect = sketched_cell_blank_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                center=(self.row * SQUARE_SIZE + SQUARE_SIZE // 2, self.col * SQUARE_SIZE + SQUARE_SIZE // 2))
             self.screen.blit(sketched_cell_blank_surf, sketched_cell_blank_rect)
 
-        if self.is_selected is True:
-            for i in range(self.col, self.col + 2):
-                pygame.draw.line(self.screen, SELECTED_COLOR, (self.row * SQUARE_SIZE, i * SQUARE_SIZE),
-                                 ((self.row + 1) * SQUARE_SIZE, i * SQUARE_SIZE), LINE_WIDTH)
+        '''if self.is_selected is True:
             for i in range(self.row, self.row + 2):
-                pygame.draw.line(self.screen, SELECTED_COLOR, (i * SQUARE_SIZE, self.col * SQUARE_SIZE),
-                                 (i * SQUARE_SIZE, (self.row + 1) * SQUARE_SIZE), LINE_WIDTH)
+                pygame.draw.line(self.screen, SELECTED_COLOR, (self.col * SQUARE_SIZE, i * SQUARE_SIZE),
+                                 ((self.col + 1) * SQUARE_SIZE, i * SQUARE_SIZE), LINE_WIDTH)
+            for i in range(self.col, self.col + 2):
+                pygame.draw.line(self.screen, SELECTED_COLOR, (i * SQUARE_SIZE, self.row * SQUARE_SIZE),
+                                 (i * SQUARE_SIZE, (self.row + 1) * SQUARE_SIZE), LINE_WIDTH)'''
